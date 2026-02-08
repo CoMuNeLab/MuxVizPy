f5fc6b87bed067894551d77b0a35e1c408472122
BaldanMatt <Baldan.matteo98@gmail.com>
2026-02-08 19:37:37 +0000

    Refactor utils.py into utils/ package with misc submodule
    Move writeComponent, readComponent, get_names into utils/misc.py
    and re-export them from utils/__init__.py for backward compatibility.

---
+++ src/MuxVizPy/utils/__init__.py
@@ -0,0 +1,10 @@
+"""General-purpose utilities: IO, config, logging, misc helpers."""
+
+from .misc import writeComponent, readComponent, get_names
+
+__all__ = [
+    "misc",
+    "writeComponent",
+    "readComponent",
+    "get_names",
+]
